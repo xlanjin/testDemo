c2a1ab4ddee32ad1cb6dec9317727e9909687ed5
xlanjin <jinxiaolan@xinongtech.com>
2019-04-28 10:47:00 +0800

    空白文件-Master

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,104 +1,70 @@
 //index.js
+
 //获取应用实例
 const app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    /* 手机号验证 */
-    ajxtrue: false,
-    name:"",
-    
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  // 手机号/固定电话验证
-  blurPhone: function (e) {
-    var phone = e.detail.value;
-    let that = this
-    let str = /^((0\d{2,3}-\d{7,8})|(1[3458]\d{9}))$/;
-    if (!(str.test(phone))) {
-      this.setData({
-        ajxtrue: false
-      })
-    } else {
-      this.setData({
-        ajxtrue: true
-      })
-      console.log('验证成功', that.data.ajxtrue)
-    }
-  },
-  blurName:function(e){
-    this.setData({
-      name:e.detail.value
-    })
-  },
-  // 表单提交
-  formSubmit(e) {
-    let that = this
-    
-    let ajxtrue = this.data.ajxtrue;
-    console.log(ajxtrue);
-    if (ajxtrue == false) {
-      //表单提交进行
-      wx.showToast({
-        title: '手机号有误',
-        icon: "none",
-        duration: 2000
-      })
-    }else{
-      let val = e.detail.value
-      console.log(val);
 
-     
-    }
   },
 
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
   onReady: function () {
-    //获得searchs组件
-    this.searchTxt = this.selectComponent("#searchTxt");
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
